bbf688bcc6404d4dc862685d6969abfc66c627c8
Keith Mattison <klmattis@ncsu.edu>
2019-04-26 23:51:56 -0400

    styling front page and layout

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import Head from 'next/head';
+import { Link } from '../routes';
 
 export default (props) => {
 	return (
@@ -8,10 +9,27 @@ export default (props) => {
 			<Head>
 				<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
 			  <link rel="stylesheet/less" type="text/css" href="/static/styles.less" />
+			  <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet" /> 
 			  <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
 			  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
 			</Head>
-			<div>Layout</div>
+			<div className="navbar">
+				<div class="header-table">
+			    <div class="tr">
+			      <div class="d1">
+							<Link route={`/`}>
+								<a>Home</a>
+							</Link>
+			      </div>
+			      <div class="d2">PORT-FOL.IO</div>
+			      <div class="d3">
+							<a href="https://github.com/Klmattis/portfolio">GitHub</a>
+							<a>Contact</a>
+			      </div>
+				  </div>
+				</div>
+			</div>
+			<div className="by-line">BY KEITH MATTISON</div>
 			{props.children}
 		</Container>
 	);
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react';
+import { Button, Card, Icon, Image } from 'semantic-ui-react';
 import Layout from '../components/layout';
 import { Link } from '../routes';
 
@@ -7,9 +7,82 @@ class Index extends Component {
 	render() {
 		return (
 			<Layout>
-				<div>
-					<h3>Campaign Module</h3>
+				<div className="large-padding-top large-padding-bottom">
+					<div className="large-text">Based on Ethereum</div>
+					<div className="large-text">Powered by Smart Contracts</div>
 				</div>
+				<div className="live-text">PROJECTS LIVE ON THE RINKEBY TEST NETWORK</div>
+				<Card.Group>
+					<Card>
+				    <Image src='/static/images/campaign-image.png' />
+				    <Card.Content>
+				      <Card.Header>CAMPAIGN</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/campaign">GitHub Repo</a></Card.Meta>
+				      <Card.Description>Crowdfund Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>Ethical Procurement</div>
+				    </Card.Content>
+				  </Card>
+			  </Card.Group>
+			  <div className="live-text">PROJECTS CURRENTLY UNDER DEVELOPMENT</div>
+			  <Card.Group>
+				  <Card>
+				    <Image src='/static/images/vitae-image.png' />
+				    <Card.Content>
+				      <Card.Header>VITAE</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/vitae">GitHub Repo</a></Card.Meta>
+				      <Card.Description>Résumé Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>Public Identification</div>
+				    </Card.Content>
+				  </Card>
+				  <Card>
+				    <Image src='/static/images/enact-image.png' />
+				    <Card.Content>
+				      <Card.Header>ENACT</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/enact">GitHub Repo</a></Card.Meta>
+				      <Card.Description>Referendum Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>Provably Fair Voting</div>
+				    </Card.Content>
+				  </Card>
+				  <Card>
+				    <Image src='/static/images/comply-image.png' />
+				    <Card.Content>
+				      <Card.Header>COMPLY</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/comply">GitHub Repo</a></Card.Meta>
+				      <Card.Description>KYC Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>FinTech Verification</div>
+				    </Card.Content>
+				  </Card>
+				  <Card>
+				    <Image src='/static/images/panacea-image.png' />
+				    <Card.Content>
+				      <Card.Header>PANACEA</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/panacea">GitHub Repo</a></Card.Meta>
+				      <Card.Description>Medical Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>Drug Attestation</div>
+				    </Card.Content>
+				  </Card>
+				  <Card>
+				    <Image src='/static/images/provenance-image.png' />
+				    <Card.Content>
+				      <Card.Header>PROVENANCE</Card.Header>
+				      <Card.Meta><a href="https://github.com/Klmattis/provenance">GitHub Repo</a></Card.Meta>
+				      <Card.Description>Supply Chain Contract</Card.Description>
+				    </Card.Content>
+				    <Card.Content extra>
+				      <div>Logistics Tracking</div>
+				    </Card.Content>
+				  </Card>
+			  </Card.Group>
 			</Layout>
 		);
 	}
