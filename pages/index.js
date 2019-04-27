c19c6a8a302f5a8f681bedf32232add567d35969
Keith Mattison <klmattis@ncsu.edu>
2019-04-27 00:19:10 -0400

    more styling

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, Icon, Image } from 'semantic-ui-react';
+import { Button, Card, Icon, Image, Item } from 'semantic-ui-react';
 import Layout from '../components/layout';
 import { Link } from '../routes';
 
@@ -7,17 +7,21 @@ class Index extends Component {
 	render() {
 		return (
 			<Layout>
-				<div className="large-padding-top large-padding-bottom">
-					<div className="large-text">Based on Ethereum</div>
-					<div className="large-text">Powered by Smart Contracts</div>
+				<div className='large-padding-top large-padding-bottom'>
+					<div className='large-text'>Based on Ethereum</div>
+					<div className='large-text'>Powered by Smart Contracts</div>
 				</div>
-				<div className="live-text">PROJECTS LIVE ON THE RINKEBY TEST NETWORK</div>
-				<Card.Group>
+				<div className='live-text'>PROJECTS LIVE ON THE RINKEBY TEST NETWORK</div>
+				<Card.Group className='portfolio-card-group'>
 					<Card>
-				    <Image src='/static/images/campaign-image.png' />
+				    <Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/campaign-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>CAMPAIGN</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/campaign">GitHub Repo</a></Card.Meta>
+				      <Card.Header>
+					      <div>CAMPAIGN</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/campaign'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>Crowdfund Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
@@ -25,13 +29,17 @@ class Index extends Component {
 				    </Card.Content>
 				  </Card>
 			  </Card.Group>
-			  <div className="live-text">PROJECTS CURRENTLY UNDER DEVELOPMENT</div>
-			  <Card.Group>
+			  <div className='live-text'>PROJECTS CURRENTLY UNDER DEVELOPMENT</div>
+			  <Card.Group className='portfolio-card-group'>
 				  <Card>
-				    <Image src='/static/images/vitae-image.png' />
+				    <Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/vitae-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>VITAE</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/vitae">GitHub Repo</a></Card.Meta>
+				    	<Card.Header>
+					      <div>VITAE</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/vitae'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>Résumé Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
@@ -39,10 +47,14 @@ class Index extends Component {
 				    </Card.Content>
 				  </Card>
 				  <Card>
-				    <Image src='/static/images/enact-image.png' />
+				    <Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/enact-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>ENACT</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/enact">GitHub Repo</a></Card.Meta>
+				      <Card.Header>
+								<div>ENACT</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/enact'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>Referendum Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
@@ -50,10 +62,14 @@ class Index extends Component {
 				    </Card.Content>
 				  </Card>
 				  <Card>
-				    <Image src='/static/images/comply-image.png' />
+				  	<Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/comply-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>COMPLY</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/comply">GitHub Repo</a></Card.Meta>
+				      <Card.Header>
+					      <div>COMPLY</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/comply'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>KYC Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
@@ -61,10 +77,14 @@ class Index extends Component {
 				    </Card.Content>
 				  </Card>
 				  <Card>
-				    <Image src='/static/images/panacea-image.png' />
+				  	<Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/panacea-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>PANACEA</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/panacea">GitHub Repo</a></Card.Meta>
+				      <Card.Header>
+					      <div>PANACEA</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/panacea'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>Medical Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
@@ -72,10 +92,14 @@ class Index extends Component {
 				    </Card.Content>
 				  </Card>
 				  <Card>
-				    <Image src='/static/images/provenance-image.png' />
+				    <Link route={`/campaign`}>
+				    	<Image as='a' src='/static/images/provenance-image.png' />
+				    </Link>
 				    <Card.Content>
-				      <Card.Header>PROVENANCE</Card.Header>
-				      <Card.Meta><a href="https://github.com/Klmattis/provenance">GitHub Repo</a></Card.Meta>
+				      <Card.Header>
+					      <div>PROVENANCE</div>
+							</Card.Header>
+				      <Card.Meta><a href='https://github.com/Klmattis/provenance'>GitHub Repo</a></Card.Meta>
 				      <Card.Description>Supply Chain Contract</Card.Description>
 				    </Card.Content>
 				    <Card.Content extra>
