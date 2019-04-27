4cf3eabe93a3c8068fc0104ea9b5037f3206ecf6
Keith Mattison <klmattis@ncsu.edu>
2019-04-26 20:12:29 -0400

    including custom less file

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,9 @@ export default (props) => {
 		<Container>
 			<Head>
 				<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"/>
+			  <link rel="stylesheet/less" type="text/css" href="/static/styles.less" />
 			  <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
+			  <script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js" ></script>
 			</Head>
 			<div>Layout</div>
 			{props.children}
